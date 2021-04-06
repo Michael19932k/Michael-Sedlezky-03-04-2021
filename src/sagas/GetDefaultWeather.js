50cc920950e689e7f4ad43dceff72110b9a3e191
Michael19932k <pucika2k@gmail.com>
2021-04-06 14:14:35 +0300

    first commit

--- src/sagas/GetDefaultWeather.js
+++ src/sagas/GetDefaultWeather.js
@@ -2,18 +2,18 @@ import { takeEvery, put, call, all } from 'redux-saga/effects';
 import { GET_DEFAULT_REQUESTED } from '../redux/actions/homeActions'
 
 function get5DayForecast(chosenCity) {
-    const get5DayForecastURL = `http://dataservice.accuweather.com/forecasts/v1/daily/5day/${chosenCity.Key}?apikey=85u8fNhlAIrpOGA8WhtltpfRbPrvWNY8&metric=true`;
-    // const get5DayForecastURL = `http://dataservice.accuweather.com/forecasts/v1/daily/5day/${chosenCity.Key}?apikey=DTjSB4rlpKHH47IXfr6C98Qm1cMlKkAr&metric=true`;
-    // const get5DayForecastURL = `http://dataservice.accuweather.com/forecasts/v1/daily/5day/${chosenCity.Key}?apikey=qN2BxxC3zI9Wrm1UaFBLQZsh84ZuLFzg&metric=true`;
+    const get5DayForecastURL = `https://dataservice.accuweather.com/forecasts/v1/daily/5day/${chosenCity.Key}?apikey=85u8fNhlAIrpOGA8WhtltpfRbPrvWNY8&metric=true`;
+    // const get5DayForecastURL = `https://dataservice.accuweather.com/forecasts/v1/daily/5day/${chosenCity.Key}?apikey=DTjSB4rlpKHH47IXfr6C98Qm1cMlKkAr&metric=true`;
+    // const get5DayForecastURL = `https://dataservice.accuweather.com/forecasts/v1/daily/5day/${chosenCity.Key}?apikey=qN2BxxC3zI9Wrm1UaFBLQZsh84ZuLFzg&metric=true`;
     return fetch(get5DayForecastURL, {
         method: 'GET',
     }).then(response => response.json())
         .catch((error) => { throw error })
 }
 function getCurrentConditions(chosenCity) {
-    const getCurrentConditionsUrl = `http://dataservice.accuweather.com/currentconditions/v1/${chosenCity.Key}?apikey=85u8fNhlAIrpOGA8WhtltpfRbPrvWNY8&language=en-us&details=false`;
-    // const getCurrentConditionsUrl = `http://dataservice.accuweather.com/currentconditions/v1/${chosenCity.Key}?apikey=DTjSB4rlpKHH47IXfr6C98Qm1cMlKkAr&language=en-us&details=false`;
-    // const getCurrentConditionsUrl = `http://dataservice.accuweather.com/currentconditions/v1/${chosenCity.Key}?apikey=qN2BxxC3zI9Wrm1UaFBLQZsh84ZuLFzg&language=en-us&details=false`;
+    const getCurrentConditionsUrl = `https://dataservice.accuweather.com/currentconditions/v1/${chosenCity.Key}?apikey=85u8fNhlAIrpOGA8WhtltpfRbPrvWNY8&language=en-us&details=false`;
+    // const getCurrentConditionsUrl = `https://dataservice.accuweather.com/currentconditions/v1/${chosenCity.Key}?apikey=DTjSB4rlpKHH47IXfr6C98Qm1cMlKkAr&language=en-us&details=false`;
+    // const getCurrentConditionsUrl = `https://dataservice.accuweather.com/currentconditions/v1/${chosenCity.Key}?apikey=qN2BxxC3zI9Wrm1UaFBLQZsh84ZuLFzg&language=en-us&details=false`;
     return fetch(getCurrentConditionsUrl, {
         method: 'GET',
     }).then(response => response.json())
